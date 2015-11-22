f575118c3f1d83b8e2679ea2da451f7014e9b89b
Jas Sohi <j450h1@gmail.com>
2015-11-22 15:58:50 -0800

    Additional questions asked in R and looked at NAs

--- Ranalysis.R
+++ Ranalysis.R
@@ -6,7 +6,6 @@ list.files()
 filename <- "businesslicenses.csv"
 df <- read.csv(filename)
 object.size(df)
-
 str(df)
 summary(df)
 View(df)
@@ -25,11 +24,36 @@ cp2015 <- cp[with(cp,grepl("^2015", IssuedDate)),]
 
 bt <- cp[with(cp,grepl("^880", Unit)),]
 
-#fastest growing cities in terms of total employees
+#Convert dates to date type
+colnames(df)
+df$IssuedDateD <- strptime(df$IssuedDate, format = "y%-%m-%d")
+df$ExpiredDateD <- strptime(df$ExpiredDate, format = "y%-%m-%d")
+df$ExtractDateD <- strptime(df$ExtractDate, format = "y%-%m-%d")
 
+# Check which columns have any NAs
+unlist(lapply(df, function(x) any(is.na(x))))
+# total number of rows
+nrow(df)
+# of NA values per column
+colSums(is.na(df)) 
+
+colnames(df)
+
+library(dplyr)
+flights %>%
+  group_by(year, month, day) %>%
+  select(arr_delay, dep_delay) %>%
+
+#fastest growing cities in terms of total employees
+  
 #what type of businesses stay in business the longest
 
+  
+  
+  
 #what type of businesses go out of business the most
+  
+# what days do most business licenses get issued
 
 
 
